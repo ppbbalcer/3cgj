65a6487160979616c04354949302ee45a008d861
Maciej Naruszewicz <maciej.naruszewicz@intel.com>
2015-01-23 23:50:46 +0100

    Move main scene to separate source file
    Signed-off-by: Maciej Naruszewicz <maciej.naruszewicz@intel.com>

--- src/SceneExamples.h
+++ src/SceneExamples.h
@@ -8,7 +8,6 @@
 #include "Engine/Engine.h"
 #include "Engine/RTexture.h"
 
-
 class SceneInit: public Scene {
 public:
 	virtual void OnLoad() {
@@ -247,128 +246,6 @@ public:
 
 private:
 	int timer;
-
-};
-
-class Scene03Object: public Scene {
-public:
-	virtual void OnLoad() {
-		bool success = EngineInst->loadResources(texturesScene03, texturesScene03Size);
-
-		_background = new RTexture(texturesScene03[1]);
-		_player = new RTexture(texturesScene03[2]);
-		_player->setPos(240, 190);
-
-		_tiles = new RTexture(texturesScene03[3]);
-		_tiles->setTileSizeSrc(32);
-		_tiles->setTileSizeDst(32);
-
-		//Load media
-		if( !success )
-		{
-			printf( "Failed to load media Scene02Renderer !\n" );
-			PAUSE();
-		}
-
-		globalAudios[1].res.sound->setVolume(0.05f);
-		globalAudios[1].res.sound->play(-1, -1);
-	}
-
-	virtual void OnFree(){
-		delete 	_background;
-		_background = NULL;
-		delete 	_player;
-		_player = NULL;
-
-		delete 	_tiles;
-		_tiles = NULL;
-
-		EngineInst->unLoadResources(texturesScene03, texturesScene03Size);
-	}
-
-	virtual void OnUpdate(int timems) {
-		//Event handler
-		SDL_Event e;
-
-		//Handle events on queue
-		while( SDL_PollEvent( &e ) != 0 )
-		{
-			Engine::eventDebug(&e);
-
-			//User requests quit, Press Esc
-			if( e.type == SDL_QUIT || (e.type == SDL_KEYDOWN  && e.key.keysym.sym == SDLK_ESCAPE) )
-			{
-				//Quit the game
-				EngineInst->breakMainLoop();
-				return;
-			}
-		}
-
-		int dist = 1.0*timems;
-		int posX = _player->getPosX();
-		int posY = _player->getPosY();
-		int width = _player->getWidth();
-
-		const Uint8* currentKeyStates = SDL_GetKeyboardState( NULL ); 
-		if( currentKeyStates[ SDL_SCANCODE_UP ] ) {
-			globalAudios[0].res.sound->play(1, 0);
-			posY -= dist;
-			if(posY < 0) {
-				posY = 0;
-			}
-		} else {
-			posY += dist;
-			if(posY > 190) {
-				posY = 190;
-			}
-		}
-
-		if( currentKeyStates[ SDL_SCANCODE_LEFT ] ) {
-			posX -= dist;
-			if (posX<0) {
-				posX = 0;
-			}
-		}
-
-		if( currentKeyStates[ SDL_SCANCODE_RIGHT ] ) {
-			posX += dist;
-			if (posX > EngineInst->screen_width() - width) {
-				posX = EngineInst->screen_width() - width;
-			}
-		}
-
-		_player->setPos(posX, posY);
-
-		EngineInst->font()->printfLT(100, 100, "Hello test text!!!");
-
-	}
-
-	virtual void OnRender(SDL_Renderer* renderer) {
-		_background->render(renderer);
-		_player->render(renderer);
-
-		int sizeDst = _tiles->getTileSizeDst() + 5;
-		int tilesNums = _tiles->getTilesNums();
-		for (int i =  0 ; i<tilesNums; ++i) {
-
-			int col = i % 20 + 2;
-			int row = i / 20 + 2;
-			
-
-			_tiles->renderTile(renderer, col * sizeDst, row * sizeDst, i);
-
-			EngineInst->font()->printfLT(col * sizeDst, row * sizeDst, "%i", i);
-
-		}
-
-	}
-
-private:
-
-	RTexture* _background;
-	RTexture* _player;
-	RTexture* _tiles;
-
 
 };
 
