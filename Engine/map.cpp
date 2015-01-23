bc11548196f0e0d96658677c752c0efc9eb706d6
maciek <maciek@debian-lenovo>
2015-01-23 19:41:18 +0100

    interfaces for map class and its implementations
    Signed-off-by: maciek <maciek@debian-lenovo>

--- Engine/map.cpp
+++ Engine/map.cpp
@@ -2,3 +2,70 @@
 
 #define MAP_WIDTH 1024
 #define MAP_HEIGHT 1024
+#include <cstdlib>
+class Field : public IField {
+  
+};
+
+class GenericMap: public IMap {
+private:
+  Field** fields;
+  int width;
+  int height;
+protected:
+  /**
+   * allocate fields
+   */
+  int AllocateFields()
+  {
+     fields = (Field**)calloc(width*height,sizeof(void*));
+  }
+  int PlaceField(int x, int y, Field * field1)
+  {
+     fields[x+y*width] = field1;
+  }
+  void DeallocateFields() {
+  for (int i = 0 ; i!=width*height; ++i ) {
+  if (fields[ i])
+    free(fields[i]);
+    }
+
+  }
+public:
+  IField* GetFieldAt(int x, int y) {
+  return fields[x+y*width];
+  }
+  GenericMap()
+  {
+    width = 0;
+    height = 0;
+    fields = 0;
+  }
+  int GetWidth() {
+    return width;
+  }
+  int GetHeight() {
+    return height;
+  }
+  
+};
+// a map loaded from file
+class LoadedMap : public GenericMap {
+public:
+  LoadedMap(char * path);
+  
+};
+
+// a map loaded from file
+class GeneratedMap : public GenericMap     {
+
+public:
+  GeneratedMap(char * generator);
+  
+};
+
+
+IMap * IMap::Factory(int type, char * parameter) {
+
+  return 0;
+}
--- Engine/map.h
+++ Engine/map.h
@@ -2,17 +2,48 @@
 #define __MAP_H
 /* following constants define width and height of map in tiles */
 
-class Field {
-
-};
-class Map {
- private:
+enum FieldType {
+  EMPTY = 0,
+  WALL = 2,
+  FLOOR,
+  DESK,
   
+  
+};
+class IField {
+public:
+  // as per enum FieldType
+  virtual int GetType() = 0;
+};
+
+
+
+class IMap {
   
  public:
-  int GetWidth();
-  int GetHeight();
-  Field* GetFieldAt();
+  /**
+   * @param type type of map to be factored (as per enum MapType)
+   * @param parameter - parameter as per map time. probably a path
+   * @return pointer to a map. If failed to generate, 0 is returned
+   *   and an exception is thrown
+   */
+  static IMap * Factory(int type, char * parameter);
+  /**
+   * @return in fields
+   */
+  virtual int GetWidth() = 0;
+  /**
+   *
+   * @return in fields
+   */
+  virtual int GetHeight() = 0;
+  /**
+   * @param x,y field position (0-n)
+   */
+  virtual IField* GetFieldAt(int x, int y) = 0;
+  virtual ~IMap() {};
 };
+
+
 
 #endif
