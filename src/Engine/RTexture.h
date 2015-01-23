9bf93daf5f2d14afadf7ab9e643ce36cf1c45d16
spawlos <spawlos@gmail.com>
2015-01-23 23:50:22 +0100

    Add scale to Texture

--- src/Engine/RTexture.h
+++ src/Engine/RTexture.h
@@ -11,10 +11,15 @@ public:
 	inline void		setPos( int x, int y );
 	inline int		getPosX();
 	inline int		getPosY();
+	inline void		setScaleSize(float scale);
+
 	inline void		render(SDL_Renderer* renderer);
 	void			renderAll(SDL_Renderer* renderer, int x, int y );
+
 	inline int		getWidth();
 	inline int		getHeight();
+	inline int		getScaleWidth();
+	inline int		getScaleHeight();
 
 	void			setTileSizeSrc(int size); //Size in file
 	void			setTileSizeDst(int size); //Size on screen
@@ -29,6 +34,8 @@ private:
 	SDL_Texture* _texture; //Image dimensions 
 	int _width; 
 	int _height;
+	int _scaleWidth; 
+	int _scaleHeight;
 	int _x; 
 	int _y;
 
@@ -52,11 +59,25 @@ int RTexture::getPosY() {
 	return _y;
 }
 
+void RTexture::setScaleSize(float scale) {
+	_scaleWidth = (int)(scale * _width); 
+	_scaleHeight = (int)(scale * _height); 
+}
+
+
 int RTexture::getWidth() {
 	return _width;
 }
 int RTexture::getHeight() {
 	return _height;
+}
+
+int RTexture::getScaleWidth() {
+	return _scaleWidth;
+}
+
+int RTexture::getScaleHeight() {
+	return _scaleHeight;
 }
 
 void RTexture::render(SDL_Renderer* renderer) {
