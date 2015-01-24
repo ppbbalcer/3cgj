6bc9fe1b64171cddfe8c2b2aeb91f93c47635076
maciej <maciej.kaminski@intel.com>
2015-01-24 13:57:31 +0100

    - check loss condition - conditional fullscreen
    Signed-off-by: maciej <maciej.kaminski@intel.com>

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ for i in `find . -name '*.o'` ; do
 done
 
 for i in `find . -name '*.cpp'` ; do
-    echo COMPILE
-     g++ -g $i -o `echo $i | sed 's/.cpp/.o/'` -Isrc  -I/usr/include/SDL2/ -c -std=gnu++11
+    echo COMPILE $i
+     g++ -g $i -o `echo $i | sed 's/.cpp/.o/'` -Isrc  -I/usr/include/SDL2/ -c -std=gnu++11 -DFULLSCREEN
 done
 echo LINK
 g++ src/*.o src/*/*.o -lSDL2 -lSDL2_mixer `sdl-config --libs`  -lSDL2_ttf -lSDL2_image -lpng -lz -o whatdowedonow 
