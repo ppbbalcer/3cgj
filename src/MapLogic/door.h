3b7925d367058fa15b7d98bd1be1ac7f7c689209
Piotr Balcer <piotr.balcer@intel.com>
2015-01-25 00:19:49 +0100

    add open doors sign & update maps

--- src/MapLogic/door.h
+++ src/MapLogic/door.h
@@ -2,6 +2,7 @@
 #define __DOOR_H
 #include "field.h"
 #include "field_impl.h"
+
 class Door: public Field {
 	int keep_open;
 	int target_board;
@@ -9,6 +10,7 @@ public:
 	Door():Field(DOOR) {keep_open=0;target_board=-1;}
 	virtual void Activate();
 	virtual void Deactivate();
+	int isOpen() { return keep_open; }
 	int GetType();
 	void SteppedOver(Character * who);
 	int GetTargetBoard() { return target_board; }
