487ed8b6a4cc49ad9ea157eb7ce9b62b1721cd89
maciej <maciej.kaminski@intel.com>
2015-01-25 03:00:31 +0100

    skulls can now be activated and deactivated
    Signed-off-by: maciej <maciej.kaminski@intel.com>

--- src/MapLogic/switch.cpp
+++ src/MapLogic/switch.cpp
@@ -27,10 +27,17 @@ void Switch::SteppedOver(Character * who) {
 	globalAudios[TOGGLE].res.sound->play();
 
 	if (associated_field) {
-		if (on_up ^ Up() )
-			ActivateRemotes();
-		else
-			DeactivateRemotes();
+		if (on_up ) {
+			if (Up())
+				ActivateRemotes();
+			else
+				DeactivateRemotes();
+		} else {
+			if (!Up())
+				ActivateRemotes();
+			else
+				DeactivateRemotes();
+		} 
 	}
 	/* update the state machine */
 	Field::SteppedOver(who);
